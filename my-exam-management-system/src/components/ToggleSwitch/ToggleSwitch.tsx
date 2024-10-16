d5d906fd524fe83a3b266c5101c066d8e195437b
NguyenThiHanh <Hanhntph34748@fpt.edu.vn>
2024-10-16 20:14:14 +0700

    commit table button status

--- my-exam-management-system/src/components/ToggleSwitch/ToggleSwitch.tsx
+++ my-exam-management-system/src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './ToggleSwitch.scss'
 interface ToggleSwitchProps {
     id: string;
-    toggleState: string;
-    onToggle: (id: string) => void; 
+    toggleState: boolean;
+    onToggle: (id: string) => void;
 }
 
 export const ToggleSwitch = ({ id, toggleState, onToggle }: ToggleSwitchProps) => (
     <div
-        className={`toggle-switch ${toggleState =='true' ? "active_" : "inactive"}`}
+        className={`toggle-switch ${toggleState == true ? "active_" : "inactive"}`}
         onClick={() => onToggle(id)}
     >
         <div className="toggle-switch-circle" />
