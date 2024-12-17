a5bd250c84a311a0c9b200d4958947d03853d700
Dan9Gooseman <dangerousopticgaming@gmail.com>
2024-12-17 09:27:54 +0700

    Thay địa chỉ api

--- my-exam-management-system/src/services/api/api.ts
+++ my-exam-management-system/src/services/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: "http://datn_be.com/",
+  baseURL: "https://wd113.websp.online/api/public/",
   headers: {
     "Content-Type": "application/json",
   },
